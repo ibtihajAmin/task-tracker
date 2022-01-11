2723741e1aed2fb65a07755fbc3dc2e3a284d95a
ibtihajAmin <ibtihaj.amin18@gmail.com>
2022-01-11 22:29:07 +0600

    Task List Table is added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import TasksListTable from "./Components/TasksListTable/TasksListTable";
 
 function App() {
+  const tasksList = [
+    { id: 1, name: "Playing Cricket", doingDate: "11/1/2022" },
+    { id: 2, name: "Watch Squid Games", doingDate: "12/1/2022" },
+    { id: 3, name: "Meet With Friends", doingDate: "13/1/2022" },
+    { id: 4, name: "Reading Novel", doingDate: "14/1/2022" },
+  ];
+
+  const [tasks, setTasks] = useState(tasksList);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center mt-3 mb-2">
+        <span className="text-info">Task</span>
+        <span className="text-primary ms-2">Tracker</span>
+      </h1>
+      <div class="row align-items-start mt-5">
+        <div class="col">
+          <h3>Add Task</h3>
+        </div>
+        <div class="col me-3">
+          <h3>Your Added Tasks</h3>
+          <TasksListTable tasks={tasks}></TasksListTable>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/TasksListTable/TasksListTable.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const TasksListTable = (props) => {
+  return (
+    <div>
+      <table class="table table-success table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Title</th>
+            <th scope="col">Date</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.tasks.map((task) => (
+            <tr key={task.id}>
+              <td>{task.name}</td>
+              <td>{task.doingDate}</td>
+              <td>
+                <button type="button" class="btn btn-warning me-3">
+                  Edit
+                </button>
+                <button type="button" class="btn btn-danger">
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TasksListTable;
